c495fdd59de16c50ac628ecff0a992a55bafe892
Julien POIRIER <connexion@julienpoirier-Webdev.com>
2023-06-14 16:06:13 +0200

    début du cours React + exo

---
+++ react/exo2/index.js
@@ -0,0 +1,14 @@
+// 1 - Créer un élément HTML de type div avec React.createElement
+
+// 2 - Selectionner la div avec l'id "root"
+
+// 3- Créer le root element
+
+// 4 - Rendre le composant dans le root element
+
+// 5 - Créer un composant React contenant un H1 avec le texte "Je suis un composant React" et deux bouttons avec le texte "Cliquez moi" et "Cliquez moi aussi"
+// 5.1 - Ajouter les classes "button red" et "button blue" aux bouttons
+// 5.2 - Ajouter la classe "container" à la div contenant le H1 et les bouttons
+// 5.3 Ajouter un évènement click sur le boutton "Cliquez moi" qui affiche une alerte avec le texte "Vous avez cliqué sur le boutton rouge"
+
+// 6 - Rendre le composant dans le root element
