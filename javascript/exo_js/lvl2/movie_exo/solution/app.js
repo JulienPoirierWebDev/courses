2a7907d07e70a85ef597c39f680fe83b7816f66e
Julien POIRIER <connexion@julienpoirier-Webdev.com>
2023-07-03 15:42:35 +0200

    update movie exo css

--- javascript/exo_js/lvl2/movie_exo/solution/app.js
+++ javascript/exo_js/lvl2/movie_exo/solution/app.js
@@ -1,12 +1,10 @@
-console.log("Hello from app.js!");
-
 import { API_KEY } from "./secret.js";
 
 function handleSubmit(event) {
   event.preventDefault();
   const input = document.querySelector("input");
   const title = input.value;
-  input.value = "";
+  //input.value = "";
 
   // if radio movie is checked
   if (document.querySelector("#movie").checked) {
@@ -29,10 +27,10 @@ function getMovies(movie) {
         const { title, poster_path, overview } = movie;
         htmlToInsert += `
 
-                <div class="movie">
+                <div class="item">
                     <h2>${title}</h2>
                     <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${title}">
-                    <p>${overview}</p>
+                    <p>${overview.substring(0, 150)}...</p>
                 </div>
             `;
       });
@@ -41,28 +39,42 @@ function getMovies(movie) {
     });
 }
 
-function getSeries(serie) {
+async function getSeries(requestString) {
+  const seriesData = await getSeriesData(requestString);
+
+  insertSeriesIntoHtml(seriesData, "results");
+}
+
+async function getSeriesData(serie) {
   const url = `https://api.themoviedb.org/3/search/tv?api_key=${API_KEY}&query=${serie}`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      const { results } = data;
-      const resultsHtml = document.querySelector("#results");
+  const response = await fetch(url);
 
-      let htmlToInsert = "";
-      results.forEach((serie) => {
-        const { name, poster_path, overview } = serie;
-        htmlToInsert += `
-                    <div class="movie">
-                        <h2>${name}</h2>
-                        <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${name}">
-                        <p>${overview}</p>
-                    </div>
-                `;
-      });
+  const { results } = await response.json();
+  return results;
+}
 
-      resultsHtml.innerHTML = htmlToInsert;
-    });
+function insertSeriesIntoHtml(series, parentTargetId) {
+  const resultsHtml = document.querySelector(`#${parentTargetId}`);
+
+  let htmlToInsert = "";
+
+  series.forEach((serie) => {
+    const newDiv = createDivMovie(serie);
+    htmlToInsert += newDiv;
+  });
+
+  resultsHtml.innerHTML = htmlToInsert;
+}
+
+function createDivMovie(serie) {
+  const { name, poster_path, overview } = serie;
+  return `
+                      <div class="movie">
+                          <h2>${name}</h2>
+                          <img src="https://image.tmdb.org/t/p/w500${poster_path}" alt="${name}">
+                          <p>${overview}</p>
+                      </div>
+                  `;
 }
 
 const searchForm = document.querySelector("#searchForm");
